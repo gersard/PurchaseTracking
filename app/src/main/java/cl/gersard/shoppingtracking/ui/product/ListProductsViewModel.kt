e571b74f36d8e01b50884099c669410dff996278
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2021-03-23 23:01:59 -0300

    - Se corrige versionado de dependencia hilt

--- app/src/main/java/cl/gersard/shoppingtracking/ui/product/ListProductsViewModel.kt
+++ app/src/main/java/cl/gersard/shoppingtracking/ui/product/ListProductsViewModel.kt
@@ -1,7 +1,11 @@
 package cl.gersard.shoppingtracking.ui.product
 
 import androidx.lifecycle.ViewModel
+import cl.gersard.shoppingtracking.domain.ProductUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ListProductsViewModel : ViewModel() {
+@HiltViewModel
+class ListProductsViewModel @Inject constructor(useCase: ProductUseCase) : ViewModel() {
     // TODO: Implement the ViewModel
 }
