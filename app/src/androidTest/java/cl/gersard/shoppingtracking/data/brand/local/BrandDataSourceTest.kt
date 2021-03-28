c368bedaeba2c63f42f21ecdf74659c700475a39
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2021-03-28 18:13:23 -0300

    - Se genera test para BrandDataSource.kt

---
+++ app/src/androidTest/java/cl/gersard/shoppingtracking/data/brand/local/BrandDataSourceTest.kt
@@ -0,0 +1,47 @@
+package cl.gersard.shoppingtracking.data.brand.local
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import cl.gersard.shoppingtracking.core.AppDatabase
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+import kotlin.jvm.Throws
+
+@RunWith(AndroidJUnit4::class)
+class BrandDataSourceTest {
+
+    private lateinit var db: AppDatabase
+    private lateinit var brandDao: BrandDao
+    private lateinit var dataSource: BrandDataSource
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        brandDao = db.brandDao()
+        dataSource = BrandDataSourceImpl(brandDao)
+    }
+
+    @Test
+    fun insertBrands() {
+        runBlocking {
+            val brandEntity = BrandUtil.fakeBrandEntity
+            val idInserted = dataSource.insertBrand(brandEntity)
+            assertEquals(brandEntity.brandId, idInserted)
+        }
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+}
---
+++ app/src/androidTest/java/cl/gersard/shoppingtracking/data/brand/local/BrandUtil.kt
@@ -0,0 +1,9 @@
+package cl.gersard.shoppingtracking.data.brand.local
+
+import cl.gersard.shoppingtracking.data.brand.BrandEntity
+
+object BrandUtil {
+
+    val fakeBrandEntity = BrandEntity(1, "Colún")
+
+}
