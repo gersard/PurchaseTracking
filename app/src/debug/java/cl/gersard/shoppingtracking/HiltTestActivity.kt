248de336be31409c9b4fcd9f7472b9d70edbd27f
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2021-04-24 15:15:11 -0400

    - Se añaden dependencias para testing - Se añade hilt activity

---
+++ app/src/debug/java/cl/gersard/shoppingtracking/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package cl.gersard.shoppingtracking
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity: AppCompatActivity() {
+}
